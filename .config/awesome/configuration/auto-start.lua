b52b766141022fdbca6df04ccb99a8f004f8ffe5
Albert24GG <guiman.albert09@gmail.com>
2022-09-03 02:13:43 +0300

    Autostart redshift for adjusting light color automatically

--- .config/awesome/configuration/auto-start.lua
+++ .config/awesome/configuration/auto-start.lua
@@ -1,1 +1,8 @@
-require("awful").spawn.with_shell("~/.config/awesome/configuration/autorun.sh")
+local awful = require("awful")
+
+-- Autostart programs
+awful.spawn.with_shell("~/.config/awesome/configuration/autorun.sh")
+
+-- Start redshift for automatic light color adjustment based on current time at your location
+local coordinates = require("configuration.user-variables").weather.coordinates
+awful.spawn.with_shell(string.format("redshift -l %s:%s", coordinates[1], coordinates[2]))
