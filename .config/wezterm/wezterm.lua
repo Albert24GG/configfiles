b595ebe9ea8f35f903fc18d6854a04f136da2fef
Albert24GG <guiman.albert09@gmail.com>
2025-11-18 17:57:46 +0200

    Use `CommitMono` nerd font in wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ local config = wezterm.config_builder()
 config.color_scheme = "nordfox"
 
 config.font_size = 13.0
-config.font = wezterm.font("CommitMono", { weight = "Bold" })
+config.font = wezterm.font("CommitMono Nerd Font", { weight = "Bold" })
 
 -- Tab bar settings
 config.hide_tab_bar_if_only_one_tab = true
