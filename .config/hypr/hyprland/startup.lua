75b2d637d04aa84e01a03e43fa53dace1adf9d72
Albert24GG <guiman.albert09@gmail.com>
2026-06-30 23:52:03 +0200

    Use uwsm session slice for background services

--- .config/hypr/hyprland/startup.lua
+++ .config/hypr/hyprland/startup.lua
@@ -1,14 +1,14 @@
 hl.on("hyprland.start", function()
 
   -- Dank Material Shell
-  hl.exec_cmd("uwsm-app -s b -- dms run")
+  hl.exec_cmd("uwsm-app -s s -- dms run")
 
   -- Screen Sharing
   hl.exec_cmd("sleep 1 && dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
 
   -- Clipboard manager
-  hl.exec_cmd("uwsm-app -s b -- clipse -listen")
+  hl.exec_cmd("uwsm-app -s s -- clipse -listen")
 
   -- Walker launcher service
-  hl.exec_cmd("uwsm-app -s b -- walker --gapplication-service")
+  hl.exec_cmd("uwsm-app -s s -- walker --gapplication-service")
 end)
